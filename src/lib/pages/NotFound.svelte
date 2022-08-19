c8a5c0ed641ba25a0fb91d9648380dbb4537922c
Adam Chen <encodeous7@gmail.com>
2022-08-18 21:52:55 -0400

    Change order.

--- src/lib/pages/NotFound.svelte
+++ src/lib/pages/NotFound.svelte
@@ -5,11 +5,10 @@
 <div class="flex h-screen">
     <div class="m-auto">
         <div class="flex flex-col rounded-2xl bg-gray-700 p-5 drop-shadow-2xl">
-
+            <div class="m-auto animate-bounce text-7xl font-black">?</div>
             <p class="text-2xl font-bold pt-4">
                 oi, where do you think you're going?
             </p>
-            <div class="m-auto animate-bounce text-7xl font-black">?</div>
             <p class="text-xl pt-3">
                 it seems that i haven't made this page yet.
                 <br/>
