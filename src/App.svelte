33588ca0908af3cb1c555fe7f472306687ef583f
Adam Chen <encodeous7@gmail.com>
2022-08-18 22:49:06 -0400

    Add transition

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,22 @@
     import FrontPage from "./lib/pages/FrontPage.svelte";
     import NotFound from "./lib/pages/NotFound.svelte";
     import { Router, Link, Route } from "svelte-routing";
-
+    import { fly } from 'svelte/transition';
     export let url = "";
 </script>
 
 <Router url="{url}">
-    <Route path="/" component="{FrontPage}"/>
+    <Route path="/" >
+       <div in:fly={{y: -20, duration: 100}} class="overflow-clip">
+           <FrontPage/>
+       </div>
+    </Route>
 <!--    <Route path="/about"></Route>-->
 <!--    <Route path="/projects" ></Route>-->
 <!--    <Route path="/stuff-i-have-done" ></Route>-->
-    <Route path="/*" component="{NotFound}"/>
+    <Route path="/*">
+        <div in:fly={{y: -20, duration: 100}} class="overflow-clip">
+            <NotFound/>
+        </div>
+    </Route>
 </Router>
